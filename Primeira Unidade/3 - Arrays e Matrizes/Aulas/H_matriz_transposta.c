6c3e198b858b1d7352b7f7e2c9fffbfb936ebc53
Henrique Figueiredo <hft@cesar.school>
2024-12-20 14:41:59 -0300

    Create H_matriz_transposta.c

---
+++ Primeira Unidade/3 - Arrays e Matrizes/Aulas/H_matriz_transposta.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main() {
+    int linhas, colunas;
+
+    // Entrada do tamanho da matriz
+    printf("Digite o número de linhas: ");
+    scanf("%d", &linhas);
+    printf("Digite o número de colunas: ");
+    scanf("%d", &colunas);
+
+    int matriz[linhas][colunas], transposta[colunas][linhas];
+
+    // Entrada dos elementos da matriz
+    printf("Digite os elementos da matriz:\n");
+    for (int i = 0; i < linhas; i++) {
+        for (int j = 0; j < colunas; j++) {
+            printf("Elemento [%d][%d]: ", i, j);
+            scanf("%d", &matriz[i][j]);
+        }
+    }
+
+    // Cálculo da matriz transposta
+    for (int i = 0; i < linhas; i++) {
+        for (int j = 0; j < colunas; j++) {
+            transposta[j][i] = matriz[i][j];
+        }
+    }
+
+    // Exibição da matriz original
+    printf("\nMatriz original:\n");
+    for (int i = 0; i < linhas; i++) {
+        for (int j = 0; j < colunas; j++) {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+
+    // Exibição da matriz transposta
+    printf("\nMatriz transposta:\n");
+    for (int i = 0; i < colunas; i++) {
+        for (int j = 0; j < linhas; j++) {
+            printf("%d ", transposta[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
